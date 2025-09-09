4b6395d1ea252b1ae1623dbd603ecf5de28dd479
Caroline Zhu <carolinezhu@microsoft.com>
2025-09-09 18:27:47 +0000

    add generate tensor inputs script

---
+++ generate-tensor-inputs.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer
+from gptqmodel import GPTQModel
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+quant_dir = "/workspace/hf_models"
+
+# Sentences we want sentence embeddings for
+sentences = ['Hello, world!']
+
+# Load model from HuggingFace Hub
+tokenizer = AutoTokenizer.from_pretrained(quant_dir)
+model = GPTQModel.from_quantized(quant_dir)
+
+# Tokenize sentences
+encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='np')
+
+print(encoded_input)
+
+np.save('input_ids', encoded_input['input_ids'])
+# np.save('token_type_ids', encoded_input['token_type_ids'])
+np.save('attention_mask', encoded_input['attention_mask'])
+
